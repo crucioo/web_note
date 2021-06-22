64d201ec7a4023dc2c055872f7f00ac7633985c6
crucioo <1225930531@qq.com>
2021-06-22 15:21:48 +0800

    简化方法

--- buildTree.js
+++ buildTree.js
@@ -1,22 +1,14 @@
-// 根据id parent_id结构化扁平数组
-// 先遍历数组全部放进一个临时对象key为id，根据id parent_id为对象里的每一项添加子节点(children)，当没有parent_id时把他放进最终返回对象(tree)里。
-function buildTree(arr) {
-  const temp = {}
-  const tree = {}
-  for (const index in arr) {
-    temp[arr[index]._id] = arr[index]
+// 最简
+var getTrees = (data) => {
+  const pIdChildMap = {}
+  data.forEach((i)=>{
+    pIdChildMap[i.parent_id] ? pIdChildMap[i.parent_id].push(i) : (pIdChildMap[i.parent_id] = [i])
+  })
+
+  for(let i = 0; i < data.length; i++) {
+    data[i].children = pIdChildMap[data[i]._id]
   }
-  for (const key in temp) {
-    if (temp[key].parent_id) {
-      if (temp[temp[key].parent_id]) {
-        if (!temp[temp[key].parent_id].children) {
-          temp[temp[key].parent_id].children = new Object()
-        }
-        temp[temp[key].parent_id].children[key] = temp[key]
-      }
-    } else {
-      tree[temp[key]._id] = temp[key]
-    }
-  }
-  console.log(tree)
+
+  // 返回成自己想要的
+  return data
 }
